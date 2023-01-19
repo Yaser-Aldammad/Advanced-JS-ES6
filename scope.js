fb9c2d54572aece751d86e9feb6a68401b4a74fa
Yaser-Aldammad <yaser_dammad@hotmail.com>
2023-01-20 00:39:33 +0100

    global vs functional scope

--- scope.js
+++ scope.js
@@ -1,2 +1,11 @@
-// funciton scope ==> var
-// Block scope ==> let, const
+// funciton scope ==> use var ==> created inside functions
+// Block scope ES6 ==> let, const
+var example = 5; // Global scope ==> parent scope: section of code that the function was defined in
+function test() {
+	var testVariable = 10; // functional scope
+	var example = 15; // funcitonal scope
+	console.log(example);
+	console.log(testVariable);
+}
+test();
+console.log(example); // global scope
