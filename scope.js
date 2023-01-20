1e436a48815201e0e15f917c321e8d4ba7c192dd
Yaser-Aldammad <yaser_dammad@hotmail.com>
2023-01-20 02:22:03 +0100

    block scope

--- scope.js
+++ scope.js
@@ -10,3 +10,38 @@ function test() {
 test();
 console.log(example); // global scope
 // expected results 15, 10, 5
+/* ==========================================================================================================
+Function Scope Hoisting==> Functions and variables declared using var are hoisted in JavaScript; that is, a function or a variable can be used before it has been declared.be careful to not use that variable before it has been assigned a value. */
+example = 7; // assigned value
+console.log(example); // expected output 7
+var example; // Declare varialbe with var. It can be hoisted ==> used before being delcared
+/*  بتعطي قيمة وبعدين بتسمي او بتبن طبيعتهvar يعني
+ */
+/* ================================================================================================================
+Block Scope is created with curely braces {} and use let & const varialbes. it could be used anywhere in the function to define new scope.
+block: let, const variables;
+Variables are not hoisted ==> can not be accessed until they are declared
+ */
+
+//Top level scope
+/* function scopeExample() {
+	//starting scope block 1
+	for (let i = 9; i < 10; i++) {
+		 Scope block 2
+	}
+	if (true) {
+		 Scope block 3
+	} else {
+		Scope block 4
+	}
+	{
+		 braces without keywords create scope block 5
+	}
+
+	// end of scope block 1
+} */
+// Top level Scope
+/* لا يمكن استعمال المتغير قبل ما تبين طبيعته , بتسميه وبعدين بتعطيه قيمةLet & const
+ */ let blockScope;
+blockScope = 50;
+console.log(blockScope); // expected results 50
