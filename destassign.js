7e38edb9fdb0a54a98d0511005d06faa9ed208e6
Yaser-Aldammad <yaser_dammad@hotmail.com>
2023-01-26 23:32:18 +0100

    update

--- destassign.js
+++ destassign.js
@@ -1,4 +1,4 @@
-Destructing assignment is syntax in JS to unpack values from Array, properties from objects, and have them into variables.
+/* Destructing assignment is syntax in JS to unpack values from Array, properties from objects, and have them into variables.
 it is powerful because it enables us to extract multiple array elements or object properties in the same expression.
 
 
@@ -6,5 +6,5 @@ A. Array Distruction:
 1. allows us to extract multiple array elements and save them into variables.
 
     in ES6 to destruct an array, we simply create an array containing the variables to assign data into and set it equal to the data array being destructed.The values in the the array are upacked and assigned to the variables in the left - hand side array from left to right, one variable per array value.
-
-    let names = []
+ */
+let names = [];
